84927e4febfb404997ec549bedc3a14d2a59bd4b
Jagjeet Singh <125492254+jagjeet-singh-23@users.noreply.github.com>
2024-07-22 18:07:23 +0530

    Update predict.py

--- ultralytics/yolo/v8/detect/predict.py
+++ ultralytics/yolo/v8/detect/predict.py
@@ -2,6 +2,8 @@
 
 import hydra
 import torch
+import playsound
+import threading
 
 from ultralytics.yolo.engine.predictor import BasePredictor
 from ultralytics.yolo.utils import DEFAULT_CONFIG, ROOT, ops
@@ -10,9 +12,16 @@ from ultralytics.yolo.utils.plotting import Annotator, colors, save_one_box
 
 
 class DetectionPredictor(BasePredictor):
+    def play_alarm_sound_function(self):
+        while True:
+            playsound.playsound(
+                "/home/frustrated-dev/fire_detection/Alarm Sound.mp3", True
+            )
 
     def get_annotator(self, img):
-        return Annotator(img, line_width=self.args.line_thickness, example=str(self.model.names))
+        return Annotator(
+            img, line_width=self.args.line_thickness, example=str(self.model.names)
+        )
 
     def preprocess(self, img):
         img = torch.from_numpy(img).to(self.model.device)
@@ -21,11 +30,13 @@ class DetectionPredictor(BasePredictor):
         return img
 
     def postprocess(self, preds, img, orig_img):
-        preds = ops.non_max_suppression(preds,
-                                        self.args.conf,
-                                        self.args.iou,
-                                        agnostic=self.args.agnostic_nms,
-                                        max_det=self.args.max_det)
+        preds = ops.non_max_suppression(
+            preds,
+            self.args.conf,
+            self.args.iou,
+            agnostic=self.args.agnostic_nms,
+            max_det=self.args.max_det,
+        )
 
         for i, pred in enumerate(preds):
             shape = orig_img[i].shape if self.webcam else orig_img.shape
@@ -41,15 +52,17 @@ class DetectionPredictor(BasePredictor):
         self.seen += 1
         im0 = im0.copy()
         if self.webcam:  # batch_size >= 1
-            log_string += f'{idx}: '
+            log_string += f"{idx}: "
             frame = self.dataset.count
         else:
-            frame = getattr(self.dataset, 'frame', 0)
+            frame = getattr(self.dataset, "frame", 0)
 
         self.data_path = p
         # save_path = str(self.save_dir / p.name)  # im.jpg
-        self.txt_path = str(self.save_dir / 'labels' / p.stem) + ('' if self.dataset.mode == 'image' else f'_{frame}')
-        log_string += '%gx%g ' % im.shape[2:]  # print string
+        self.txt_path = str(self.save_dir / "labels" / p.stem) + (
+            "" if self.dataset.mode == "image" else f"_{frame}"
+        )
+        log_string += "%gx%g " % im.shape[2:]  # print string
         self.annotator = self.get_annotator(im0)
 
         det = preds[idx]
@@ -63,27 +76,52 @@ class DetectionPredictor(BasePredictor):
         gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
         for *xyxy, conf, cls in reversed(det):
             if self.args.save_txt:  # Write to file
-                xywh = (ops.xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                line = (cls, *xywh, conf) if self.args.save_conf else (cls, *xywh)  # label format
-                with open(f'{self.txt_path}.txt', 'a') as f:
-                    f.write(('%g ' * len(line)).rstrip() % line + '\n')
+                xywh = (
+                    (ops.xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn)
+                    .view(-1)
+                    .tolist()
+                )  # normalized xywh
+                line = (
+                    (cls, *xywh, conf) if self.args.save_conf else (cls, *xywh)
+                )  # label format
+                with open(f"{self.txt_path}.txt", "a") as f:
+                    f.write(("%g " * len(line)).rstrip() % line + "\n")
 
-            if self.args.save or self.args.save_crop or self.args.show:  # Add bbox to image
+            if (
+                self.args.save or self.args.save_crop or self.args.show
+            ):  # Add bbox to image
+                threading.Thread(target=self.play_alarm_sound_function).start()
                 c = int(cls)  # integer class
-                label = None if self.args.hide_labels else (
-                    self.model.names[c] if self.args.hide_conf else f'{self.model.names[c]} {conf:.2f}')
+                label = (
+                    None
+                    if self.args.hide_labels
+                    else (
+                        self.model.names[c]
+                        if self.args.hide_conf
+                        else f"{self.model.names[c]} {conf:.2f}"
+                    )
+                )
                 self.annotator.box_label(xyxy, label, color=colors(c, True))
             if self.args.save_crop:
                 imc = im0.copy()
-                save_one_box(xyxy,
-                             imc,
-                             file=self.save_dir / 'crops' / self.model.model.names[c] / f'{self.data_path.stem}.jpg',
-                             BGR=True)
+                save_one_box(
+                    xyxy,
+                    imc,
+                    file=self.save_dir
+                    / "crops"
+                    / self.model.model.names[c]
+                    / f"{self.data_path.stem}.jpg",
+                    BGR=True,
+                )
 
         return log_string
 
 
-@hydra.main(version_base=None, config_path=str(DEFAULT_CONFIG.parent), config_name=DEFAULT_CONFIG.name)
+@hydra.main(
+    version_base=None,
+    config_path=str(DEFAULT_CONFIG.parent),
+    config_name=DEFAULT_CONFIG.name,
+)
 def predict(cfg):
     cfg.model = cfg.model or "yolov8n.pt"
     cfg.imgsz = check_imgsz(cfg.imgsz, min_dim=2)  # check image size
